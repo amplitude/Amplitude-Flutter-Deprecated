234e589c127e4487a54f39b3968c792659d7cc6c
Alex Daniel <adaniel@carbonfive.com>
2019-04-09 13:23:20 -0700

    Implement Android platform code to log events without properties.

--- android/src/main/java/com/amplitude/amplitude_flutter/AmplitudeFlutterPlugin.java
+++ android/src/main/java/com/amplitude/amplitude_flutter/AmplitudeFlutterPlugin.java
@@ -1,23 +1,33 @@
 package com.amplitude.amplitude_flutter;
 
+import android.util.Log;
+
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
 import io.flutter.plugin.common.PluginRegistry.Registrar;
 
+import com.amplitude.api.Amplitude;
+
 /** AmplitudeFlutterPlugin */
 public class AmplitudeFlutterPlugin implements MethodCallHandler {
   /** Plugin registration. */
   public static void registerWith(Registrar registrar) {
     final MethodChannel channel = new MethodChannel(registrar.messenger(), "amplitude_flutter");
+
+    Amplitude.getInstance().initialize(registrar.context(), "API_KEY");
+    Amplitude.getInstance().enableForegroundTracking(registrar.activity().getApplication());
+
     channel.setMethodCallHandler(new AmplitudeFlutterPlugin());
   }
 
   @Override
   public void onMethodCall(MethodCall call, Result result) {
-    if (call.method.equals("getPlatformVersion")) {
-      result.success("Android " + android.os.Build.VERSION.RELEASE);
+    if (call.method.equals("logEvent")) {
+      final String eventName = call.argument("name");
+      Amplitude.getInstance().logEvent(eventName);
+      result.success(null);
     } else {
       result.notImplemented();
     }
