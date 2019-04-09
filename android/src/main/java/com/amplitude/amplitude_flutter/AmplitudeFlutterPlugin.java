30d49b1f2bba740994e7344803370e694a215728
Alex Daniel <adaniel@carbonfive.com>
2019-04-09 10:33:44 -0700

    Generate amplitude flutter plugin package using `flutter create`.

---
+++ android/src/main/java/com/amplitude/amplitude_flutter/AmplitudeFlutterPlugin.java
@@ -0,0 +1,25 @@
+package com.amplitude.amplitude_flutter;
+
+import io.flutter.plugin.common.MethodCall;
+import io.flutter.plugin.common.MethodChannel;
+import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
+import io.flutter.plugin.common.MethodChannel.Result;
+import io.flutter.plugin.common.PluginRegistry.Registrar;
+
+/** AmplitudeFlutterPlugin */
+public class AmplitudeFlutterPlugin implements MethodCallHandler {
+  /** Plugin registration. */
+  public static void registerWith(Registrar registrar) {
+    final MethodChannel channel = new MethodChannel(registrar.messenger(), "amplitude_flutter");
+    channel.setMethodCallHandler(new AmplitudeFlutterPlugin());
+  }
+
+  @Override
+  public void onMethodCall(MethodCall call, Result result) {
+    if (call.method.equals("getPlatformVersion")) {
+      result.success("Android " + android.os.Build.VERSION.RELEASE);
+    } else {
+      result.notImplemented();
+    }
+  }
+}
---
+++ ios/Classes/AmplitudeFlutterPlugin.m
@@ -0,0 +1,20 @@
+#import "AmplitudeFlutterPlugin.h"
+
+@implementation AmplitudeFlutterPlugin
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"amplitude_flutter"
+            binaryMessenger:[registrar messenger]];
+  AmplitudeFlutterPlugin* instance = [[AmplitudeFlutterPlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
+}
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
+@end
