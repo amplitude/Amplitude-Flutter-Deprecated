5601f0a93d9c70feaaa818ba7a588d0489db8879
Ingar Shu <ingar@carbonfive.com>
2019-04-17 16:07:08 -0700

    Use more descriptive method names for EventStore

--- lib/event_store.dart
+++ lib/event_store.dart
@@ -8,12 +8,12 @@ class EventStore {
 
   num get length => events.length;
 
-  void add(Map<String, dynamic> event) {
+  void enqueue(Map<String, dynamic> event) {
     event['timestamp'] = DateTime.now().millisecondsSinceEpoch;
     events.add(event);
   }
 
-  List<Map<String, dynamic>> pop(int count) {
+  List<Map<String, dynamic>> dequeue(int count) {
     assert(count >= 0);
 
     final endRange = min(count, events.length);
--- test/event_store_test.dart
+++ test/event_store_test.dart
@@ -13,20 +13,20 @@ void main() {
       test('returns the length of events in the store', () {
         expect(subject.length, equals(0));
 
-        subject.add(<String, dynamic>{'name': 'event 1'});
+        subject.enqueue(<String, dynamic>{'name': 'event 1'});
         expect(subject.length, equals(1));
 
-        subject.add(<String, dynamic>{'name': 'event 2'});
+        subject.enqueue(<String, dynamic>{'name': 'event 2'});
         expect(subject.length, equals(2));
       });
     });
 
-    group('.add', () {
+    group('.enqueue', () {
       test('adds an event to the store, and adds a timestamp property', () {
-        subject.add(<String, dynamic>{'name': 'event 1'});
+        subject.enqueue(<String, dynamic>{'name': 'event 1'});
         expect(subject.length, equals(1));
 
-        final List<Map<String, dynamic>> events = subject.pop(1);
+        final List<Map<String, dynamic>> events = subject.dequeue(1);
         final Map<String, dynamic> event = events[0];
 
         expect(event['timestamp'], isInstanceOf<int>());
@@ -37,17 +37,17 @@ void main() {
       test('removes a specified number of the oldest events in the store', () {
         expect(subject.length, equals(0));
 
-        subject.add(<String, dynamic>{'name': 'event 1'});
-        subject.add(<String, dynamic>{'name': 'event 2'});
-        subject.add(<String, dynamic>{'name': 'event 3'});
+        subject.enqueue(<String, dynamic>{'name': 'event 1'});
+        subject.enqueue(<String, dynamic>{'name': 'event 2'});
+        subject.enqueue(<String, dynamic>{'name': 'event 3'});
         expect(subject.length, equals(3));
 
-        final List<Map<String, dynamic>> firstTwoEvents = subject.pop(2);
+        final List<Map<String, dynamic>> firstTwoEvents = subject.dequeue(2);
         expect(firstTwoEvents.length, equals(2));
         expect(firstTwoEvents[0]['name'], equals('event 1'));
         expect(firstTwoEvents[1]['name'], equals('event 2'));
 
-        final List<Map<String, dynamic>> lastEvent = subject.pop(1);
+        final List<Map<String, dynamic>> lastEvent = subject.dequeue(1);
         expect(lastEvent.length, equals(1));
         expect(lastEvent[0]['name'], equals('event 3'));
 
@@ -55,11 +55,11 @@ void main() {
       });
 
       test('works with numbers greater than the event count', () {
-        subject.add(<String, dynamic>{'name': 'event 1'});
-        subject.add(<String, dynamic>{'name': 'event 2'});
+        subject.enqueue(<String, dynamic>{'name': 'event 1'});
+        subject.enqueue(<String, dynamic>{'name': 'event 2'});
         expect(subject.length, equals(2));
 
-        final List<Map<String, dynamic>> poppedEvents = subject.pop(100);
+        final List<Map<String, dynamic>> poppedEvents = subject.dequeue(100);
         expect(poppedEvents.length, equals(2));
       });
     });
