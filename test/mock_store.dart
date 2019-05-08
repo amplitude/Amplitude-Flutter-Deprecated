37026ce211f5f10f8984de8f63457c09b0020686
Ingar Shu <ingar@carbonfive.com>
2019-05-08 10:46:36 -0700

    Update the MockStore so that flushing and event counts are correct.

--- test/mock_store.dart
+++ test/mock_store.dart
@@ -1,21 +1,25 @@
-import 'package:amplitude_flutter/src/store.dart';
 import 'package:amplitude_flutter/src/event.dart';
+import 'package:amplitude_flutter/src/store.dart';
 
 class MockStore implements Store {
   @override
   int length = 0;
+  int curId = 10000;
 
   final List<Event> db = <Event>[];
 
   @override
   Future<int> add(Event event) {
+    event.id = ++curId;
     db.add(event);
     length++;
     return Future.value(0);
   }
 
   @override
-  Future<void> delete(List<int> eventIds) async {}
+  Future<void> delete(List<int> eventIds) async {
+    db.removeWhere((Event event) => eventIds.contains(event.id));
+  }
 
   @override
   Future<void> empty() async {
