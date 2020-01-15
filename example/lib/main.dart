5a0a1d48253efb79ed77c3fbc0953ca45b5539a7
haoliu <hao.liu@amplitude.com>
2020-01-15 12:52:30 -0800

    Fix some issues and removed file not needed

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -2,4 +2,4 @@ import 'package:flutter/material.dart';
 
 import 'my_app.dart';
 
-void main() => runApp(const MyApp('5f26af770436ca6d4f501458de77edbb'));
+void main() => runApp(const MyApp('API_KEY'));
--- lib/src/device_info_helper.dart
+++ lib/src/device_info_helper.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-
 import 'package:flutter/services.dart';
 
 class DeviceInfoHelper {
