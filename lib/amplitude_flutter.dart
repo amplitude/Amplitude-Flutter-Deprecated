26bb7776120e756d7f0d749033ae5327bbcab7df
Reinaldo Moreira <reinaldo.moreira@nubank.com.br>
2020-03-10 10:57:11 -0300

    expose client and service provider
    (cherry picked from commit a48c3ee6a10d612e8ab687ae0960321b7381dac1)

--- lib/amplitude_flutter.dart
+++ lib/amplitude_flutter.dart
@@ -1,6 +1,8 @@
 library amplitude_flutter;
 
 export 'src/amplitude_flutter.dart';
+export 'src/client.dart';
 export 'src/config.dart';
 export 'src/identify.dart';
 export 'src/revenue.dart';
+export 'src/service_provider.dart';
